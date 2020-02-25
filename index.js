bad20ba2536661de4e7210249ec00f8d51c495f1
FelixMo42-school <felixmoses@students.berkeley.net>
2020-02-25 14:26:19 -0800

    draw a tirangel!

--- index.js
+++ index.js
@@ -29,16 +29,16 @@ function createProgram(gl, vertexShader, fragmentShader) {
     gl.deleteProgram(program)
 }
 
-function resize(canvas) {
+function resize(gl) {
     // Lookup the size the browser is displaying the canvas.
-    var displayWidth  = canvas.clientWidth
-    var displayHeight = canvas.clientHeight
+    var displayWidth  = gl.canvas.clientWidth
+    var displayHeight = gl.canvas.clientHeight
    
     // Check if the canvas is not the same size.
-    if (canvas.width  !== displayWidth || canvas.height !== displayHeight) {
+    if (gl.canvas.width !== displayWidth || gl.canvas.height !== displayHeight) {
         // Make the canvas the same size
-        canvas.width  = displayWidth
-        canvas.height = displayHeight
+        gl.canvas.width  = displayWidth
+        gl.canvas.height = displayHeight
     }
 }
 
@@ -75,13 +75,13 @@ function clear(gl) {
 async function main() {
     let canvas = document.getElementById("canvas")
 
-    resize(canvas)
-
     let gl = canvas.getContext("webgl2", {
         antialias: true,
         alpha: false,
         desynchronized: true
     })
+
+    resize(gl)
 
     let vertexShader = createShader( gl, gl.VERTEX_SHADER,  await get("./shader/vertex.glsl") )
     let fragmentShader = createShader( gl, gl.FRAGMENT_SHADER, await get("./shader/fragment.glsl") )
@@ -95,6 +95,7 @@ async function main() {
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
 
     var vao = gl.createVertexArray()
+
     gl.bindVertexArray(vao)
     gl.enableVertexAttribArray(positionAttributeLocation)
 
@@ -105,47 +106,38 @@ async function main() {
     var offset = 0        // start at the beginning of the buffer
     gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset)
 
-    var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution")
-    var colorLocation = gl.getUniformLocation(program, "u_color")
+    let geomerty;
 
-    var primitiveType = gl.TRIANGLES
-    var offset = 0
-    var count = 3
-    gl.drawArrays(primitiveType, offset, count)
+    function setGeometry(data) {
+        if ( data.length % 2 != 0 ) throw "geomerty must be even"
 
-    // var colorLocation = gl.getUniformLocation(program, "u_color");
+        geometry = new Float32Array(data)
+    }
+
+    setGeometry([
+        0, 0,
+        0, 0.5,
+        0.5, 0,
+    ])
 
     function mainLoop() {
         // clear the canvas
         clear(gl)
 
-        // Tell it to use our program (pair of shaders)
+        // Tell it to use our program and select the buffer and all that
         gl.useProgram(program)
-
-        // Bind the attribute/buffer set we want.
         gl.bindVertexArray(vao)
-
-
-        // Set a the color.
-        gl.uniform4fv(colorLocation, color)
-
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
 
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-            0, 0,
-            0, 0.5,
-            0.7, 0,
-        ]), gl.STATIC_DRAW)
+        gl.bufferData(gl.ARRAY_BUFFER, geometry, gl.STATIC_DRAW)
 
         let offset = 0
-        let count = 6
+        let count = geometry.length / 2
 
         gl.drawArrays(gl.TRIANGLES, offset, count)
     }
 
-    mainLoop()
+    window.setInterval(mainLoop(), 500)
 }
 
-main()
-
-// $(main)
+$(main)
--- shader/fragment.glsl
+++ shader/fragment.glsl
@@ -4,9 +4,6 @@
 // to pick one. mediump is a good default. It means "medium precision"
 precision mediump float;
 
-// get a color as input
-uniform vec4 u_color;
-
 // we need to declare an output for the fragment shader
 out vec4 outColor;
 
